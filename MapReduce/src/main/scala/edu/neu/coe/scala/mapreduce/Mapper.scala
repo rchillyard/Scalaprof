5622d559792cf807efef36f664c65c0f729f7f9b
Scala Prof <scalaprof@gmail.com>
2015-11-20 11:33:49 -0500

    MapReduce: refactored mapper (replaced for-comprehension with call to map)

--- MapReduce/src/main/scala/edu/neu/coe/scala/mapreduce/Mapper.scala
+++ MapReduce/src/main/scala/edu/neu/coe/scala/mapreduce/Mapper.scala
@@ -17,8 +17,7 @@ class Mapper[X,K,V](f: (X)=>Map[K,V]) extends Actor with ActorLogging {
   override def receive = {
     case xs: Seq[X] =>
       log.info(s"received Seq[X]: with ${xs.length} elements")
-      val r = for {x <- xs} yield f(x)
-      sender ! Shuffle(r)
+      sender ! Shuffle(xs map f)
     case x: Any =>
       log.warning(s"received unknown message type: {}",x)
   }
