7e6ae65d01d262ed33f1ffe2d2978ea475da1cc0
Scala Prof <scalaprof@gmail.com>
2015-11-30 10:29:52 -0500

    RPN: * is now configurable (for type of calculator) * fixed favicon issue

--- RPN/app/controllers/Application.scala
+++ RPN/app/controllers/Application.scala
@@ -16,29 +16,34 @@ import models._
 
 class Application extends Controller {
   
-//  val config = ConfigFactory.load()
+  val config = ConfigFactory.load()
+  val which = config.getString("calculator")
   
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
   implicit val timeout: Timeout = Timeout(10 seconds)
-  implicit val system = ActorSystem("RPN-Calculator")  
-  val setup = Application.getSetupForRational
+  implicit val system = ActorSystem("RPN-Calculator")
+  val setup = which match {
+    case "rational" => Application.getSetupForRational
+    case "double" => Application.getSetupForDouble
+    case _ => Console.err.println(s"Unsupported calculator type: $which"); Application.getSetupForRational
+  }
   val calculator = system.actorOf(setup _1,setup _2)
- 
-  val prefix: String = setup _3
+  val name: String = setup _3;
+  println(s"$name is ready")
 
   def index() = Action.async {
     val xsf = (calculator ? View).mapTo[Seq[_]]
     xsf map {
-      case xs => Ok(s"$prefix: calculator has the following elements (starting with top): $xs")
+      case xs => Ok(s"$name: calculator has the following elements (starting with top): $xs")
     }
   }
 
   def command(s: String) = Action.async {
     val xtf = (calculator ? s).mapTo[Try[_]] 
     xtf map {
-      case Success(x) => Ok(s"""$prefix: you entered "$s" and got back $x""")
-      case Failure(e) => if (s=="clr") Ok("ScalaRPN: cleared") else Ok(s"""ScalaRPN: you entered "$s" which caused error: $e""")
-//      case Failure(e) => if (s=="clr") redirect("/") else  Ok(s"""ScalaRPN: you entered "$s" which caused error: $e""")
+      case Success(x) => Ok(s"""$name: you entered "$s" and got back $x""")
+      case Failure(e) => if (s=="clr") Ok("$name: cleared") else Ok(s"""$name: you entered "$s" which caused error: $e""")
+//      case Failure(e) => if (s=="clr") redirect("/") else  Ok(s"""$name: you entered "$s" which caused error: $e""")
     }
   }
 
@@ -49,17 +54,18 @@ object Application {
 		  implicit val lookup: String=>Option[Double] = DoubleMill.constants.get _
       implicit val conv: String=>Try[Double] = DoubleMill.valueOf _
 			implicit val parser = new ExpressionParser[Double](conv,lookup)
-      println(s"creating double calculator with parser: ${parser.toString()}")
 			val mill: Mill[Double] = DoubleMill()
 			// Note: the following pattern should NOT be used within an actor
       val props = Props(new Calculator(mill,parser))
 			(props,"doubleCalculator","Double Calculator")
   }
+  // CONSIDER This assumes that we have Rational in our classpath already.
+  // I'd like to try the possibility of dynamically loading the Rational stuff.
+  // But, that's going to be very tricky, so we'll leave it for now.
     def getSetupForRational(implicit system: ActorSystem) = {
       implicit val lookup: String=>Option[Rational] = RationalMill.constants.get _
       implicit val conv: String=>Try[Rational] = RationalMill.valueOf _
       implicit val parser = new ExpressionParser[Rational](conv,lookup)
-      println(s"creating double calculator with parser: ${parser.toString()}")
       val mill: Mill[Rational] = RationalMill()
       // Note: the following pattern should NOT be used within an actor
       val props = Props(new Calculator(mill,parser))
