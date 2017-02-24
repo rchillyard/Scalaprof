659bb1ba0502dbdf794e07351c0034f4ac37f8c2
Scala Prof <scalaprof@gmail.com>
2017-02-24 17:23:44 -0500

    Updates for CSYE7200-35939 Assignment 5

--- FunctionalProgramming/src/main/scala/edu/neu/coe/scala/crawler/HTMLParser.scala
+++ FunctionalProgramming/src/main/scala/edu/neu/coe/scala/crawler/HTMLParser.scala
@@ -3,23 +3,24 @@ package edu.neu.coe.scala.crawler
 import scala.util.Try
 
 /**
- * @author scalaprof
- */
+  * @author scalaprof
+  */
 object HTMLParser {
+
+  import java.io.ByteArrayInputStream
+
+  import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+  import org.xml.sax.InputSource
+
   import scala.xml.Node
   import scala.xml.parsing.NoBindingFactoryAdapter
 
-  import org.xml.sax.InputSource
-  import java.io.ByteArrayInputStream
-
-  import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl 
-
   lazy val adapter = new NoBindingFactoryAdapter()
   lazy val parser = (new SAXFactoryImpl).newSAXParser
-  
-  def parse(html: String, encoding: String = "UTF-8"): Try[Node] = Try(this.parse(html.getBytes(encoding)))
 
-  def parse(html: Array[Byte]): Node = {
+  def parse(html: String, encoding: String = "UTF-8"): Try[Node] = this.parse(html.getBytes(encoding))
+
+  def parse(html: Array[Byte]): Try[Node] = Try{
     val stream = new ByteArrayInputStream(html)
     val source = new InputSource(stream)
     adapter.loadXML(source, parser)
